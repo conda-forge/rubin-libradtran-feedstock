d38d353276f3e405fb490373118f599b80f6df4c
Eli Rykoff <erykoff@stanford.edu>
2024-08-25 10:49:00 -0700

    Use different python path when cross-compiling.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,12 @@ set -x
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./
 
-# We need to explicitly set the python path
-export ac_cv_path_PYTHON=$PREFIX/bin/python
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+    export ac_cv_path_PYTHON=$BUILD_PREFIX/bin/python
+else
+    # We need to explicitly set the python path
+    export ac_cv_path_PYTHON=$PREFIX/bin/python
+fi
 
 rm -f src/.depend
 rm -f libsrc_c/.depend
